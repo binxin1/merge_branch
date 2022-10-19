99e977c743a43a6325f3af7730c5e09d1aab3e50
pengbin1 <pengbin1@360.cn>
2022-10-19 17:33:25 +0800

    [git_test1] add print_wohahaha function

--- wohahaha.cc
+++ wohahaha.cc
@@ -1,7 +1,13 @@
 #include <iostream>
 
+int print_wohaha() {
+    std::cout << "wohaha" << std::endl; 
+    return 0; 
+}
+
+
 int main() {
     std::cout << "hello world" << std::endl; 
     
-    return 0;
+    return 0; 
 }
